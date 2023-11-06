e4f740b6c24810cd9ad569836cf5da3490a2a54c
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-11-06 01:09:37 -0500

    added: database connection and fetching data

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,32 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const moongose = require('mongoose');
+
+
 const router = express.Router();
+dotenv.config()
+
+
+const dbUser = process.env.DB_USER
+const database = process.env.DATABASE
+const password = process.env.DB_PASSWORD
+
+const url = `mongodb+srv://${dbUser}:${password}@cluster0.uq02s3f.mongodb.net/${database}?retryWrites=true&w=majority`
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await moongose.connect(url);
+}
+
+const MessageSchema = new moongose.Schema({
+  user: String,
+  message: String,
+  added: Date
+})
+
+const MessageCollection = moongose.model('Message', MessageSchema)
+
 
 // default message
 
@@ -17,8 +44,19 @@ const messages = [
 ]
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Messageboard', messages: messages })
+router.get('/', async function(req, res, next) {
+
+  try {
+
+    const messages = await MessageCollection.find()
+
+    res.render('index', { title: 'Mini Messageboard', messages: messages })
+
+  } catch (err) {
+    console.error(err)
+    next(err)
+  }
+
 })
 
 router.get('/new', function(req, res, next) {
@@ -29,9 +67,26 @@ router.post('/new', async (req, res) => {
   let messageText = req.body.txtMessage
   let usertText = req.body.txtAuthor
 
-  messages.push({ text: messageText, user: usertText, added: new Date() })
 
-  res.redirect('/')
+  const messageDetail = new MessageCollection({
+    user: usertText,
+    message: messageText,
+    added: new Date()
+  })
+
+  try {
+    const messageSave = await messageDetail.save()
+    if (messageDetail) {
+      res.redirect('/')
+    } else {
+
+    }
+
+  } catch (err) {
+    console.error(err)
+  }
+
+
 
 })
 
